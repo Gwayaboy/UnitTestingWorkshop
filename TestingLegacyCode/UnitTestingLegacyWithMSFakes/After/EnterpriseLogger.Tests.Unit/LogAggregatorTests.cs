41f348c2a73a6460d0170b804221c02d2c8334c2
Franck Théolade <ftheolade@gmail.com>
2020-12-14 16:58:46 +0000

    Added after MS Fakes exampe

---
+++ TestingLegacyCode/UnitTestingLegacyWithMSFakes/After/EnterpriseLogger.Tests.Unit/LogAggregatorTests.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation" file="LogAggregatorTests.cs">
+//   Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.
+// </copyright>
+// <summary>
+//   The LogAggregatorTests
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace EnterpriseLogger.Tests.Unit
+{
+    using System;
+    using System.Fakes;
+    using System.IO.Fakes;
+    using Microsoft.ALMRangers.FakesGuide.EnterpriseLogger;
+    using Microsoft.QualityTools.Testing.Fakes;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LogAggregatorTests
+    {
+        [TestMethod]
+        public void AggregateLogs_PastThreeDays_ReturnsAllLinesFromPastThreeDays()
+        {
+            using (ShimsContext.Create())
+            {
+                // Arrange
+                var sut = new LogAggregator();
+                ShimDirectory.GetFilesStringString = (dir, pattern) => new[] 
+                {
+                    @"C:\someLogDir\Log_20121001.log",
+                    @"C:\someLogDir\Log_20121002.log",
+                    @"C:\someLogDir\Log_20121005.log"
+                };
+                ShimFile.ReadAllLinesString = path =>
+                {
+                    switch (path)
+                    {
+                        case @"C:\someLogDir\Log_20121001.log":
+                            return new[] { "OctFirstLine1", "OctFirstLine2" };
+                        case @"C:\someLogDir\Log_20121002.log":
+                            return new[] { "ThreeDaysAgoFirstLine", "OctSecondLine2" };
+                        case @"C:\someLogDir\Log_20121005.log":
+                            return new[] { "OctFifthLine1", "TodayLastLine" };
+                    }
+
+                    return new string[] { };
+                };
+                ShimDateTime.TodayGet = () => new DateTime(2012, 10, 05);
+
+                // Act
+                var result = sut.AggregateLogs(@"C:\SomeLogDir", daysInPast: 3);
+                
+                // Assert
+                Assert.AreEqual(4, result.Length, "Number of aggregated lines incorrect.");
+                CollectionAssert.Contains(result, "ThreeDaysAgoFirstLine", "Expected line missing from aggregated log.");
+                CollectionAssert.Contains(result, "TodayLastLine", "Expected line missing from aggregated log.");
+            }
+        }
+    }
+}
---
+++ TestingLegacyCode/UnitTestingLegacyWithMSFakes/After/EnterpriseLogger/LogAggregator.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Microsoft Corporation" file="LogAggregator.cs">
+//   Copyright Microsoft Corporation. All Rights Reserved. This code released under the terms of the Microsoft Public License (MS-PL, http://opensource.org/licenses/ms-pl.html.) This is sample code only, do not use in production environments.
+// </copyright>
+// <summary>
+//   The LogAggregator
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Microsoft.ALMRangers.FakesGuide.EnterpriseLogger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+
+    public class LogAggregator
+    {
+        public string[] AggregateLogs(string logDirPath, int daysInPast)
+        {
+            var mergedLines = new List<string>();
+            var filePaths = Directory.GetFiles(logDirPath, "*.log");
+            foreach (var filePath in filePaths)
+            {
+                if (this.IsInDateRange(filePath, daysInPast))
+                {
+                    mergedLines.AddRange(File.ReadAllLines(filePath));
+                }
+            }
+
+            return mergedLines.ToArray();
+        }
+
+        private bool IsInDateRange(string filePath, int daysInPast)
+        {
+            string logName = Path.GetFileNameWithoutExtension(filePath);
+            if (logName.Length < 8)
+            {
+                return false;
+            }
+
+            string logDayString = logName.Substring(logName.Length - 8, 8);
+            DateTime logDay;
+            DateTime today = DateTime.Today;
+            if (DateTime.TryParseExact(logDayString, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out logDay))
+            {
+                return logDay.AddDays(daysInPast) >= today;
+            }
+
+            return false;
+        }
+    }
+}
