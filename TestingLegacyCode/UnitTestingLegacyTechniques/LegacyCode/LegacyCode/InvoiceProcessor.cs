4eefe878808dee94fe2d58e53ddbc8eee3f4dff3
Gwayaboy <frtheola@microsoft.com>
2022-02-21 09:17:13 +0000

    Legacy invoice processor wrap method back to initial state

--- TestingLegacyCode/UnitTestingLegacyTechniques/LegacyCode/LegacyCode/InvoiceProcessor.cs
+++ TestingLegacyCode/UnitTestingLegacyTechniques/LegacyCode/LegacyCode/InvoiceProcessor.cs
@@ -6,17 +6,6 @@ namespace LegacyCode
     {
         public void SendInvoice()
         {
-            DoStuffForInvoices();
-            Logging();
-        }
-
-        public void Logging()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void DoStuffForInvoices()
-        {
             //Code to get invoice data
             var invoice = ServiceLocator.InvoiceService.GetInvoice();
             #region do some more stuff with the invoice data
@@ -26,6 +15,6 @@ namespace LegacyCode
             #region do some prep work before sending the invoice
             #endregion
             ServiceLocator.EmailService.SendInvoice(invoice);
-        }
+        }       
     }
 }
